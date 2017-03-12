91ffe0ee530b82428ecc7f501b7441ab4c8ef980
zein12 <jena.zein@gmail.com>
2017-03-13 06:13:53 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,10 +3,10 @@
 // Line bot API で払いだされた情報を記載
 //https://developers.line.me/channels/
 $config['account'] = [
-	'Name'          => '', // 今のところなくても問題ない。
-	'MID'           => '', 
-	'ChannelID'     => '' ,
-	'ChannelSecret' => '',
+	'Name'          => 'Tes', // 今のところなくても問題ない。
+	'MID'           => '3/cEBpOR0mjAMUtnHKrSrx3N6FnMVNPYfXBIwMO6HNGaljxuxTxZz2fGrmZYFwqfV3dvAWMa7FEGrmOONfbZ7or1wxYgpjbtFMS0Mkk+RftjvYSrUpThxAHGiivf2M662z2zM5P8BSKby0dJiBG3GQdB04t89/1O/w1cDnyilFU=', 
+	'ChannelID'     => '1497601714' ,
+	'ChannelSecret' => ' a6b4b1a80d9f25eb0a719fc92cef7d86 ',
 ]  ;
 
 
