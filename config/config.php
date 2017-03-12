83bcc587b80f61ef508a3ae7397724794c8a9ea1
zein12 <jena.zein@gmail.com>
2017-03-13 06:20:59 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 //https://developers.line.me/channels/
 $config['account'] = [
 	'Name'          => 'Tes', // 今のところなくても問題ない。
-	'MID'           => '3/cEBpOR0mjAMUtnHKrSrx3N6FnMVNPYfXBIwMO6HNGaljxuxTxZz2fGrmZYFwqfV3dvAWMa7FEGrmOONfbZ7or1wxYgpjbtFMS0Mkk+RftjvYSrUpThxAHGiivf2M662z2zM5P8BSKby0dJiBG3GQdB04t89/1O/w1cDnyilFU=', 
+	'MID'           => 'U402dc714e18504d318618e09af1f5951', 
 	'ChannelID'     => '1497601714' ,
 	'ChannelSecret' => ' a6b4b1a80d9f25eb0a719fc92cef7d86 ',
 ]  ;
